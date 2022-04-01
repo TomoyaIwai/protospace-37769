e1c8c205a3c484902f166fc2aaa476832c9d4d24
TomoyaIwai <tomoyanbaby@gmail.com>
2022-04-01 16:10:57 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'prototypes/index'
+  root to: "prototypes#index"
+
+  resources :prototypes do
+    resources :comments
+  end
+
+  resources :users
+
+end
